34005a5382e31b89b19c385734e21e1f2d5b5e96
Beatriz <beatrizslcam@gmail.com>
2019-04-02 07:34:25 -0400

    Exercicios da semana 1

---
+++ Desktop/URI/1180_Menor_e_Posicao.cpp
@@ -0,0 +1,42 @@
+/*aça um programa que leia um valor N. Este N será o tamanho de um vetor X[N].
+ A seguir, leia cada um dos valores de X, encontre o menor elemento deste vetor
+  e a sua posição dentro do vetor, mostrando esta informação.
+
+Entrada
+A primeira linha de entrada contem um único inteiro N (1 < N < 1000),
+indicando o número de elementos que deverão ser lidos em seguida para o vetor
+X[N] de inteiros. A segunda linha contém cada um dos N valores, separados por um espaço.
+
+Saída
+A primeira linha apresenta a mensagem “Menor valor:” seguida de um espaço
+e do menor valor lido na entrada. A segunda linha apresenta a mensagem “Posicao:”
+seguido de um espaço e da posição do vetor na qual se encontra o menor valor lido,
+lembrando que o vetor inicia na posição zero.*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    int menor, p;
+
+
+    cin>>N;
+    int X[N];
+    for(int i = 0; i< N; i++){
+        cin>> X[i];
+        if(i==0){
+            menor= X[i];
+            p=i;
+        }
+        else if(X[i]<menor){
+            menor=X[i];
+            p=i;
+        }
+    }
+    cout<<"Menor valor: "<< menor << "\nPosicao: "<<p<<"\n";
+
+
+    return 0;
+}
